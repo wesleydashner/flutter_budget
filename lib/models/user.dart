e657694a20c66eb29dd3acce20c68791b3e69fd0
Wesley Dashner <wdashner11@gmail.com>
2020-03-07 11:59:29 -0700

    user model

---
+++ lib/models/user.dart
@@ -0,0 +1,7 @@
+import 'package:budget/models/account.dart';
+
+class User {
+  final List<Account> accounts;
+
+  User({this.accounts});
+}
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,18 +1,19 @@
 import 'package:budget/models/account.dart';
+import 'package:budget/models/user.dart';
 import 'package:budget/screens/home/widgets/account_card.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  final accounts = <Account>[
+  final user = User(accounts: <Account>[
     Account(name: 'Investments', amount: 0),
     Account(name: 'Usable', amount: 141.39),
     Account(name: 'Taxes', amount: 42.5),
-  ];
+  ]);
 
   @override
   Widget build(BuildContext context) {
     var accountCards = <Widget>[];
-    for (final account in accounts) {
+    for (final account in user.accounts) {
       accountCards.add(AccountCard(
         name: account.name,
         amount: account.amount,
