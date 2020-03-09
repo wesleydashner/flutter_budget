5c39dda3c54ea46bcbc54274d2aabfc3cbb98213
Wesley Dashner <wdashner11@gmail.com>
2020-03-08 21:58:22 -0600

    backspace works when entering amount

--- lib/screens/home/widgets/money_text_input_formatter.dart
+++ lib/screens/home/widgets/money_text_input_formatter.dart
@@ -2,10 +2,19 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 
 class MoneyTextInputFormatter extends TextInputFormatter {
+  // this works because default value in account card TextEditingController is 0.00
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
-    // this works because default value in account card TextEditingController is 0.00
+    debugPrint('oldValue: ${oldValue.text}');
+    debugPrint('newValue: ${newValue.text}');
+    // if backspace
+    if (oldValue.text.substring(0, oldValue.text.length - 1) == newValue.text &&
+        oldValue.text != '0.00') {
+      return TextEditingValue(
+        text: (double.parse(newValue.text) / 10).toStringAsFixed(2),
+      );
+    }
     return TextEditingValue(
       text: (double.parse(newValue.text) * 10).toStringAsFixed(2),
     );
